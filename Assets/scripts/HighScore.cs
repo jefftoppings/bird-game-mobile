197026a748652ebe1b70589c5a144500626d875c
Jeff Toppings <jeffrey.toppings@outlook.com>
2019-12-22 16:21:14 -0600

    Add highscore script

---
+++ Assets/scripts/HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighScore : MonoBehaviour
+{
+    private Text highscore;
+
+    private void OnEnable()
+    {
+        highscore = GetComponent<Text>();
+        highscore.text = PlayerPrefs.GetInt("HighScore").ToString();
+    }
+}
